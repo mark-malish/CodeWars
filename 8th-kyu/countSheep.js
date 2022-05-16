80211939b897887a9db5bc78630117e2bec96c4a
Mark Malish <mark.malish@gmail.com>
2022-05-15 21:42:37 -0500

    filters array for truthy values

---
+++ 8th-kyu/countSheep.js
@@ -0,0 +1,8 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+    let sheep = arrayOfSheep.filter(value => value === true).length
+    return sheep
+}
+
+// return arrayOfSheep.filter(Boolean).length;
